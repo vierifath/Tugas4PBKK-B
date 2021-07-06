b7d0b0ab2c286d9d12955a84a80368cbad09e06e
vierifath <vieri.ayuba@gmail.com>
2021-07-07 02:47:11 +0700

    add

---
+++ tugas5_pbkk_vieri/tugas5_pbkk_vieri/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace tugas5_pbkk_vieri
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
